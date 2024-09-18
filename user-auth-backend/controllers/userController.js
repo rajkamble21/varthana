61c23452f99831730f70f79211189f69847f3b42
Raj Kamble <rajskamble21@gmail.com>
2024-09-18 18:47:09 +0530

    UpdateUserById route added

--- user-auth-backend/controllers/userController.js
+++ user-auth-backend/controllers/userController.js
@@ -17,6 +17,20 @@ const deleteUserById = async (req, res) => {
     }
 }
 
+const updateUserById = async (req, res) => {
+    try {
+        let user = await findUserById(req.params.id);
+        if (!user) {
+            return res.status(404).json({ message: `User with id:${req.params.id} not found !` });
+        }
+        let newUser = await updateUser(req.params.id, req.body);
+        return res.status(200).json({ message: 'User updated successfully', newUser })
+    } catch (error) {
+        return res.status(500).json({ message: error.message, error })
+    }
+}
+
 module.exports = {
-    deleteUserById
+    deleteUserById,
+    updateUserById
 }
--- user-auth-backend/routes/userRoute.js
+++ user-auth-backend/routes/userRoute.js
@@ -1,9 +1,10 @@
 const router = require("express").Router();
 const { getAllUsers } = require('../controllers/authController');
-const { deleteUserById } = require('../controllers/userController');
+const { deleteUserById, updateUserById } = require('../controllers/userController');
 const auth = require('../middlewares/auth')
 
 router.get('/', auth, getAllUsers);
+router.patch('/:id', auth, updateUserById);
 router.delete('/:id', auth, deleteUserById);
 
 module.exports = router;
